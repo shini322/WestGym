fa1e2c2cd1c2b4f572aef728b538aaeb406a3ee7
shini322 <33940422+shini322@users.noreply.github.com>
2020-08-18 00:14:16 +0300

    Start project

---
+++ app/js/main.js
@@ -0,0 +1,6 @@
+$('.trainers__slider').slick({
+    slidesToShow: 3,
+    centerMode: true,
+    nextArrow: '<button class="slider-arrows slider-arrows__next"></button>',
+    prevArrow: '<button class="slider-arrows slider-arrows__prev"></button>',
+});
