db38377ffffb1b872c973551c3ec57ef79fbc0bf
shini322 <33940422+shini322@users.noreply.github.com>
2020-08-18 08:49:11 +0300

    Done

--- app/js/main.js
+++ app/js/main.js
@@ -3,4 +3,42 @@ $('.trainers__slider').slick({
     centerMode: true,
     nextArrow: '<button class="slider-arrows slider-arrows__next"></button>',
     prevArrow: '<button class="slider-arrows slider-arrows__prev"></button>',
+    responsive: [
+        {
+          breakpoint: 1600,
+          settings: {
+            slidesToShow: 2,
+          }
+        },
+        {
+            breakpoint: 1250,
+            settings: {
+              slidesToShow: 1,
+            }
+          },
+        
+    ]
 });
+
+$('.nav-toggle').click(function() {
+    $(this).toggleClass('opened');
+    $('.header__menu').slideToggle()
+});
+
+document.addEventListener('click', function(e) {
+    var map = document.querySelector('.contacts__map')
+    if(e.target.id === 'contacts__map') {
+        map.style.pointerEvents = 'all'
+    } else {
+        map.style.pointerEvents = 'none'
+    }
+  })
+
+  document.addEventListener('click', function(e) {
+    var table = document.querySelector('.price__table')
+    if(e.target.id === 'price__table') {
+        table.style.pointerEvents = 'all'
+    } else {
+        table.style.pointerEvents = 'none'
+    }
+  })
