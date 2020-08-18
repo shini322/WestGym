455c942972ec7373840e0d0db477666331a75db7
shini322 <33940422+shini322@users.noreply.github.com>
2020-08-18 11:27:02 +0300

    Add anchors

--- app/js/main.js
+++ app/js/main.js
@@ -64,3 +64,12 @@ function showYaMaps(){
  script.src   = "https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3Ac0a7bb6f633ae5e9d206346727914b341ce954f59ef3e267f2fdc0d778ea8f5e&amp;width=100%25&amp;height=683&amp;lang=ru_RU&amp;scroll=true";
  document.getElementById("YaMaps").appendChild(script);
 }
+
+$(".menu__list li a").on("click",  function (event) {
+    event.preventDefault();
+    $('.header__menu').slideToggle()
+    $('.nav-toggle').toggleClass('opened')
+    var id  = $(this).attr('href'),
+        top = $(id).offset().top;
+    $('body,html').animate({scrollTop: top}, 400);
+});
