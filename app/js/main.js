a6b8a94418fe8d95fcb2bb32643eb0db44664618
shini322 <33940422+shini322@users.noreply.github.com>
2020-08-18 10:49:54 +0300

    Fix map 3

--- app/js/main.js
+++ app/js/main.js
@@ -26,7 +26,7 @@ $('.nav-toggle').click(function() {
 });
 
 document.addEventListener('click', function(e) {
-    var map = document.querySelector('.contacts__map')
+    var map = document.querySelector('.contacts__map > ymaps')
     if(e.target.id === 'contacts__map') {
         map.style.pointerEvents = 'all'
     } else {
@@ -47,9 +47,7 @@ document.addEventListener('click', function(e) {
   YaMapsShown = false;
 
 
-$(document).ready(function (){
-
- $(window).scroll(function() {
+  $(window).scroll(function() {
     if (!YaMapsShown){
      if($(window).scrollTop() + $(window).height() > $(document).height() - 1000) {      
       showYaMaps();
@@ -57,13 +55,12 @@ $(document).ready(function (){
      }
     }
  });
- 
-});
 
 
 function showYaMaps(){
  var script   = document.createElement("script");
  script.type  = "text/javascript";
- script.src   = "https://yandex.ru/map-widget/v1/?um=constructor%3Ac0a7bb6f633ae5e9d206346727914b341ce954f59ef3e267f2fdc0d778ea8f5e&amp;source=constructor";
+ 
+ script.src   = "https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3Ac0a7bb6f633ae5e9d206346727914b341ce954f59ef3e267f2fdc0d778ea8f5e&amp;width=100%25&amp;height=683&amp;lang=ru_RU&amp;scroll=true";
  document.getElementById("YaMaps").appendChild(script);
 }
